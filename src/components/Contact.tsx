6b6463017f6b01654462e6073bf40151d70316ec
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-10-28 10:10:04 +0900

    add file

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./assets/contact.scss";
+import "../assets/contact.scss";
 import InstagramIcon from "@material-ui/icons/Instagram"
 import GitHubIcon from "@material-ui/icons/GitHub";
 import TwitterIcon from "@material-ui/icons/Twitter";
