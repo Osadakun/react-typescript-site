1dea4a9384ff745bccd507108264c2f647070508
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-11-10 23:16:25 +0900

    fix

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import { Router } from './components/Router';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header';
-import './home.css';
+import 'src/assets/home.scss';
 
 function Home() {
   return (
